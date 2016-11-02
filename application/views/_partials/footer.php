aff731a7bb225f8c42e35bb9d879d7f0cc12ac43
James Stewart <jointgapjames514@yahoo.com>
2016-11-02 14:09:08 +0800

    update ci config

--- application/views/_partials/footer.php
+++ application/views/_partials/footer.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<?php if (ENVIRONMENT=='development'): ?>
 			<p class="pull-right text-muted">
-				CI Bootstrap Version: <strong><?php echo CI_BOOTSTRAP_VERSION; ?></strong>, 
+				CI Bootstrap Version: <strong><?php echo CI_SCRAPPYGRAM; ?></strong>, 
 				CI Version: <strong><?php echo CI_VERSION; ?></strong>, 
 				Elapsed Time: <strong>{elapsed_time}</strong> seconds, 
 				Memory Usage: <strong>{memory_usage}</strong>
