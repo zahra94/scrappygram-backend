a6a89ea5e4dbb1e06c5912d59b81c53b5f26c8f2
James <jointgapjames514@yahoo.com>
2016-11-15 01:06:23 +0800

    Change APP Base with Postgresql

--- config.php
+++ config.php
@@ -4,10 +4,10 @@ if (strpos($_SERVER['SERVER_NAME'], "herokuapp.com") !== false) {
 	
 	$DBconfig = array(
 	    "database_type"=>"pgsql",
-	    "database_name"=>"d155nktfa27sm4",
-	    "server"=>"ec2-107-22-250-212.compute-1.amazonaws.com",
-	    "username"=>"vuvsizazbuhdgy",
-	    "password"=>"kpbxfYeR_z63n2AHSfsBgAeOFO",
+	    "database_name"=>"d5bvn9hj6ekmcb",
+	    "server"=>"ec2-52-86-23-161.compute-1.amazonaws.com",
+	    "username"=>"u4b4ck2pr818qd",
+	    "password"=>"p9e2eik71mcicm6vvedvmb0is79",
 	    "charset"=>"utf8",
 	    "port"=>5432
 	);
